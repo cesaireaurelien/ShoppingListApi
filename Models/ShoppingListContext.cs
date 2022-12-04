be4e7277970239d514542f5230c05a3a124ab781
cngaleu <cesaire.ngaleu@ceratec.com>
2022-12-04 15:08:39 -0500

    Build API

---
+++ Models/ShoppingListContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShoppingListApi.Models
+{
+    public class ShoppingListContext: DbContext
+    {
+        public DbSet<Grocery> Grocery { get; set; }
+        public ShoppingListContext(DbContextOptions<ShoppingListContext> options): base(options)
+        {
+
+        }
+    }
+}
